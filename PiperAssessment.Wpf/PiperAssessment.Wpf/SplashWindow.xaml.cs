58940015cf342d276ccc47bcdcee7d82c427c2b7
j.c.hubbell0713@gmail.com <j.c.hubbell0713@gmail.com>
2023-07-14 09:26:12 -0500

    Initialize project

---
+++ PiperAssessment.Wpf/PiperAssessment.Wpf/SplashWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace PiperAssessment.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+            LoadMainWindow();
+        }
+        public void LoadMainWindow()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(3000);
+
+                Dispatcher.Invoke(() =>
+                {
+                    var mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    Close();
+                });
+            });
+        }
+    }
+}
